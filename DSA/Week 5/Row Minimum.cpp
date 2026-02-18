6cc1ee3592869071559dc473f4d6ff3405bed5ce
shubhamsinghbundela <shubhamkrsingh11@gmail.com>
2026-02-18 11:33:08 +0530

    row minimum problem - 18-02-2026

---
+++ DSA/Week 5/Row Minimum.cpp
@@ -0,0 +1,51 @@
+// You are given an integer matrix of size n×m
+// .
+
+// Your task is to compute the minimum element of each row.
+
+// Input
+// The first line contains two integers n
+//  and m
+//  (1≤n,m≤1000
+// ). The next n
+//  lines each contain m
+//  integers ai,j
+//  (0≤ai,j≤105
+// ), representing the matrix.
+
+// Output
+// Print n
+//  integers separated by spaces, the minimum element of each row in order.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int n;
+    cin>>n;
+    int m;
+    cin>>m;
+    int arr[n][m];
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin>>arr[i][j];
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        int min = arr[i][0];
+        for (int j = 0; j < m; j++) {
+            if(arr[i][j]<min){
+                min = arr[i][j];
+            }
+        }
+        cout<<min<<" ";
+    }
+}
+
+int main() {
+	int testCases=1;
+// 	cin>>testCases;
+    for(int i=0;i<testCases;i++){
+        solve();
+    }
+}
