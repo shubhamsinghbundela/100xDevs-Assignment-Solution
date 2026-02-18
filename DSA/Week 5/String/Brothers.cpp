07552c296d06d25514ba3c9463ab332a8dd17cf0
shubhamsinghbundela <shubhamkrsingh11@gmail.com>
2026-02-18 14:11:37 +0530

    Brother - 18-02-2026

---
+++ DSA/Week 5/String/Brothers.cpp
@@ -0,0 +1,17 @@
+// You are given the names of two people.
+
+// Each name is given as two words: FirstName LastName.
+
+// Determine whether they are brothers.
+
+// They are considered brothers if and only if their last names are the same.
+
+// Input
+// The input consists of two lines.
+
+// Each line contains two strings: FirstName and LastName. ' All strings contain only English letters and have length from 1
+//  to 100
+// .
+
+// Output
+// Print YES if they are brothers, otherwise print NO.
