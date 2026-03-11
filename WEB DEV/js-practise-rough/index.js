fb86002a8385c742ab7ce72479590aee26de1161
shubhamsinghbundela <shubhamsigdar@gmail.com>
2026-03-11 07:52:35 +0530

    added stuff

--- WEB DEV/js-practise-rough/index.js
+++ WEB DEV/js-practise-rough/index.js
@@ -1,17 +1,48 @@
-let results = [];
-let limit = 2;
-let items=[2,10,2]
-const worker = (delay, cb) => setTimeout(() => cb(null, `Finished ${delay}`), delay);
+function createWindowAggregator(windowSize, onWindowReady) {
+  let arr = [];
+  let l=0, r=windowSize-1;
+  return function(...args){
+     //On each new value, compute and emit the current average.
+    //  Before N values are received, compute the average
+     arr.push(...args);
+     if(arr.length<windowSize){
+        let sum = 0;
+        for(let i=0; i<arr.length;i++){
+            sum+=arr[i];
+        }
 
-for (let i = 0; i <= limit; i++) {
-  worker(items[i], (err, data) => {
-    
+        let avg = sum/arr.length;
+        onWindowReady(avg);
+     }else{
+       while(r<arr.length){
+        // console.log(r);
+          let sum = 0;
+          for(let i=l; i<=r; i++){
+            sum+=arr[i];
+          }
+          console.log(sum);
 
-    results.push(data);
-    console.log('results', results)
-
-  });
-  console.log('results', results)
+          let avg =  sum/windowSize;
+          onWindowReady(avg);
+          l+=1;
+          r+=1;
+       }
+     }
+  }
 }
 
-console.log("Loop finished");
+const results = [];
+    
+    const onWindowReady = (avg) => {
+      results.push(avg);
+      if (results.length === 4) {
+        console.log(results);
+      }
+    };
+
+    const add = createWindowAggregator(3, onWindowReady);
+
+    add(1);
+    add(2);
+    add(3);
+    add(10);
